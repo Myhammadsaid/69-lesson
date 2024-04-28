3eb49c1d97ca564f5323a24ff98a38eb7546eb10
Mуhammadsaid <muhammadsaidmm4@gmail.com>
2024-04-28 13:22:03 +0300

    appdat

---
+++ src/components/data/data.js
@@ -0,0 +1,51 @@
+import product1 from "../../assets/images/product-img1.png";
+import product2 from "../../assets/images/product-img2.png";
+import product3 from "../../assets/images/product-img3.png";
+import product4 from "../../assets/images/product-img4.png";
+import product5 from "../../assets/images/product-img5.png";
+import product6 from "../../assets/images/product-img6.png";
+
+export let data = [
+  {
+    id: 1,
+    text: "Vegie Muffen",
+    price: 16,
+    par: "There are many things are needed to start the Fast Food Business.",
+    bg: product1,
+  },
+  {
+    id: 2,
+    text: "Salads",
+    price: 12,
+    par: "There are many things are needed to start the Fast Food Business.",
+    bg: product2,
+  },
+  {
+    id: 3,
+    text: "Burger",
+    price: 10,
+    par: "There are many things are needed to start the Fast Food Business.",
+    bg: product3,
+  },
+  {
+    id: 4,
+    text: "Delmonico Steak dish",
+    price: 14,
+    par: "There are many things are needed to start the Fast Food Business.",
+    bg: product4,
+  },
+  {
+    id: 5,
+    text: "Egg Masala",
+    price: 9,
+    par: "There are many things are needed to start the Fast Food Business.",
+    bg: product5,
+  },
+  {
+    id: 6,
+    text: "Peach Melba dish",
+    price: 15,
+    par: "There are many things are needed to start the Fast Food Business.",
+    bg: product6,
+  },
+];
--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
-import product1 from "../../assets/images/product-img1.png";
-import product2 from "../../assets/images/product-img2.png";
-import product3 from "../../assets/images/product-img3.png";
-import product4 from "../../assets/images/product-img4.png";
-import product5 from "../../assets/images/product-img5.png";
-import product6 from "../../assets/images/product-img6.png";
 import star from "../../assets/images/stars.png";
+import { data } from "../data/data";
 import "./Product.css";
 
 export default class Product extends Component {
@@ -22,108 +17,22 @@ export default class Product extends Component {
               specialized equipment, Skills to manage Customers,{" "}
             </p>
             <div className="product-cards">
-              <div className="product-card">
-                <img src={product1} alt="product-img" />
-                <div className="product-card-content">
-                  <div className="product-box">
-                    <p className="product-c-text">Vegie Muffen</p>
-                    <p className="product-c-text">16$</p>
-                  </div>
-                  <p className="product-c-par">
-                    There are many things are needed to start the Fast Food
-                    Business.
-                  </p>
-                  <div className="product-rating">
-                    <button className="product-btn">+</button>
-                    <img src={star} alt="" />
-                  </div>
-                </div>
-              </div>
-              <div className="product-card">
-                <img src={product2} alt="product-img" />
-                <div className="product-card-content">
-                  <div className="product-box">
-                    <p className="product-c-text">Salads</p>
-                    <p className="product-c-text">12$</p>
-                  </div>
-                  <p className="product-c-par">
-                    There are many things are needed to start the Fast Food
-                    Business.
-                  </p>
-                  <div className="product-rating">
-                    <button className="product-btn">+</button>
-                    <img src={star} alt="" />
+              {data?.map((el) => (
+                <div key={el.id} className="product-card">
+                  <img src={el.bg} alt="product-img" />
+                  <div className="product-card-content">
+                    <div className="product-box">
+                      <p className="product-c-text">{el.text}</p>
+                      <p className="product-c-text">{el.price}$</p>
+                    </div>
+                    <p className="product-c-par">{el.par}</p>
+                    <div className="product-rating">
+                      <button className="product-btn">+</button>
+                      <img src={star} alt="" />
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="product-card">
-                <img src={product3} alt="product-img" />
-                <div className="product-card-content">
-                  <div className="product-box">
-                    <p className="product-c-text">Burger</p>
-                    <p className="product-c-text">10$</p>
-                  </div>
-                  <p className="product-c-par">
-                    There are many things are needed to start the Fast Food
-                    Business.
-                  </p>
-                  <div className="product-rating">
-                    <button className="product-btn">+</button>
-                    <img src={star} alt="" />
-                  </div>
-                </div>
-              </div>
-              <div className="product-card">
-                <img src={product4} alt="product-img" />
-                <div className="product-card-content">
-                  <div className="product-box">
-                    <p className="product-c-text">Delmonico Steak dish</p>
-                    <p className="product-c-text">14$</p>
-                  </div>
-                  <p className="product-c-par">
-                    There are many things are needed to start the Fast Food
-                    Business.
-                  </p>
-                  <div className="product-rating">
-                    <button className="product-btn">+</button>
-                    <img src={star} alt="" />
-                  </div>
-                </div>
-              </div>
-              <div className="product-card">
-                <img src={product5} alt="product-img" />
-                <div className="product-card-content">
-                  <div className="product-box">
-                    <p className="product-c-text">Egg Masala</p>
-                    <p className="product-c-text">9$</p>
-                  </div>
-                  <p className="product-c-par">
-                    There are many things are needed to start the Fast Food
-                    Business.
-                  </p>
-                  <div className="product-rating">
-                    <button className="product-btn">+</button>
-                    <img src={star} alt="" />
-                  </div>
-                </div>
-              </div>
-              <div className="product-card">
-                <img src={product6} alt="product-img" />
-                <div className="product-card-content">
-                  <div className="product-box">
-                    <p className="product-c-text">Peach Melba dish</p>
-                    <p className="product-c-text">15$</p>
-                  </div>
-                  <p className="product-c-par">
-                    There are many things are needed to start the Fast Food
-                    Business.
-                  </p>
-                  <div className="product-rating">
-                    <button className="product-btn">+</button>
-                    <img src={star} alt="" />
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
             <button className="product-btn2">Learn More</button>
           </div>
